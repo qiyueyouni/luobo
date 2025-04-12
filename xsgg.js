0648c0dfacbdd888ad392c2aac9978fbcc18842a
qiyueyouni <479783013@qq.com>
2025-04-12 12:39:06 +0800

    更新 xsgg.js

--- xsgg.js
+++ xsgg.js
@@ -1,16 +1,9 @@
 const url = "https://cdn.jsdelivr.net/gh/bgvioletsky/QX@0.2.0/rewrite_remote/xbs/render.html";
 
-$httpClient.get({
-    url: url
-}, function(error, response, data) {
+$httpClient.get(url, function(error, response, data) {
     if (error) {
-        console.log("请求失败：" + error);
         $done();
     } else {
-        $done({
-            response: {
-                bodyBytes: response.body // Surge 返回的二进制数据在 body 里
-            }
-        });
+        $done({response: {body: data}});
     }
 });
