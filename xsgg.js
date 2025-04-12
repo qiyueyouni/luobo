57892e4f125272b6e364669175e4b89cce443fea
qiyueyouni <479783013@qq.com>
2025-04-12 13:33:38 +0800

    Delete xsgg.js

--- xsgg.js
+++
@@ -1,9 +0,0 @@
-const url = "https://cdn.jsdelivr.net/gh/bgvioletsky/QX@0.2.0/rewrite_remote/xbs/render.html";
-
-$httpClient.get(url, function(error, response, data) {
-    if (error) {
-        $done();
-    } else {
-        $done({response: {body: data}});
-    }
-});
