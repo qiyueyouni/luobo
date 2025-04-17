ae539308d7e57763224e994e8f356b2cf8fce75e
qiyueyouni <479783013@qq.com>
2025-04-17 10:55:05 +0800

    更新 Base64toImage.js

--- tool/Base64toImage.js
+++ tool/Base64toImage.js
@@ -1,4 +1,4 @@
-const url = "https://raw.githubusercontent.com/qiyueyouni/luobo/main/tool/base64toimg.html";
+const url = "https://raw.githubusercontent.com/qiyueyouni/woshiluobo_tool/main/index.html";
 
 $httpClient.get(url, function(error, response, data) {
     if (error) {
